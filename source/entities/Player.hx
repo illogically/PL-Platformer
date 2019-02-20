7c30bccb2490f7e5b6ec28515607df75f1029ad0
illogically <illogicaldevs@gmail.com>
2019-02-20 14:38:52 +0800

    Added Variable Jump Height

--- source/entities/Player.hx
+++ source/entities/Player.hx
@@ -13,27 +13,31 @@ import flixel.system.debug.watch.Tracker.TrackerProfile;
 class Player extends FlxSprite 
 {
 	var SPEED:Float = 500;
-	var JUMP:Float = -600;
+	var JUMP:Float = -350;
 	var GRAVITY:Float;
 	
 	public function new(?X:Float=0, ?Y:Float=0) 
 	{
 		super(X, Y);
 		
-		GRAVITY = JUMP * -3;
+		GRAVITY = JUMP * -5;
 		
-		drag.y = JUMP * -5;
+		drag.y = JUMP * -10;
 		acceleration.y = GRAVITY;
 		maxVelocity.x = 350;
+		maxVelocity.y = 700;
 	}
 	
 	override public function update(elapsed:Float){	
 		updateMove(elapsed);
+		if (k.anyJustPressed([R])){
+			FlxG.log.clear();
+		}
 		super.update(elapsed);
 	}
 	
 	var jumpTmr:Float = 0;
-	var jumpNum:Int = 0;
+	var jumpState:Int = 0;
 	
 	function updateMove(elapsed){
 		var _right:Bool = k.anyPressed([RIGHT, D]);
@@ -72,18 +76,38 @@ class Player extends FlxSprite
 		
 		if (_jump){
 			jumpTmr += elapsed;
-			if (isTouching(0x1000) && jumpTmr <= 0.02){
-				velocity.y = JUMP;
-				jumpNum++;
-				FlxG.log.add(jumpNum);
-			} else if (jumpTmr >= 0.25){
-				acceleration.y = GRAVITY;
-				FlxG.log.add(jumpNum);
+			if (isTouching(0x1000) && jumpState == 0){
+				jumpState = 1;
+				jumpTmr = 0;
+				FlxG.log.add("Init Jump");
 			}
+			if (jumpTmr >= 2){
+				jumpState = -1;
+				FlxG.log.add("Jump Time Out");
+			}
+			if (jumpState == -1){
+				FlxG.log.add("Jump End");
+				acceleration.y = GRAVITY;
+				if (isTouching(0x1000)){
+					jumpTmr = 0;
+				}
+			}
+			
+			if (jumpState == 1 ){
+				if (jumpTmr <= 0.3){
+					velocity.y = JUMP;	
+					FlxG.log.add("Small Jump");
+				} else {
+					drag.y = JUMP * -10 * jumpTmr;
+					FlxG.log.add("Extended Jump");
+				}
+			}
+			
 		} else {
+			jumpState = 0;
 			acceleration.y = GRAVITY;
 			jumpTmr = 0;
-			jumpNum = 0;
+			jumpState = 0;
 		}
 	}
 }
